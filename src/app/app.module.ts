06fddbed0fe3ab47d8d77891df2cc97e234e915d
Your Name <yefferson.barrial@avivel.com.pe>
2024-01-06 16:47:13 -0500

    change global & manually language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -6,6 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
+// CONFIGURACIÓN DEL LOCALE DE LA APP
+import localeEsPE from '@angular/common/locales/es-PE';
+import localeFrCA from '@angular/common/locales/fr-CA';
+import localeItVA from '@angular/common/locales/it-VA';
+
+// REGISTRAR TODOS LOS IDIOMAS A MANEJAR(importados)
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEsPE );
+registerLocaleData( localeFrCA );
+registerLocaleData( localeItVA );
 
 
 @NgModule({
@@ -18,7 +29,11 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-PE'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
