2eb59859fd340a423aff0677dba2aec360df2ed2
Your Name <yefferson.barrial@avivel.com.pe>
2024-01-11 09:23:04 -0500

    i18nPlural Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -21,4 +21,20 @@ export class UncommonPageComponent {
     this.gender = 'female';
   }
 
+  // i18nPlural
+  public clients: string[] = ['Maria','Pedro','Fernando','Hernando','Eduardo','Melissa','Natalia'];
+  public clientsMap = {
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    'other': 'tenemos # clientes esperando',
+  }
+
+
+  deleteClient() {
+    this.clients.shift();
+  }
+
+
+
 }
