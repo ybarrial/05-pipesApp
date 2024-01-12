77645ca90513ac286a3a458bf9314f033836caf4
Your Name <yefferson.barrial@avivel.com.pe>
2024-01-12 17:24:04 -0500

    valores y argumentos personalizados al Pipe Toggle

--- src/app/products/pipes/toggle-case.pipe.ts
+++ src/app/products/pipes/toggle-case.pipe.ts
@@ -5,8 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ToggleCasePipe implements PipeTransform {
 
-  transform(value: string): string {
-    return value.toUpperCase();
+  transform(value: string, toUpper: boolean = false): string {
+    console.log({value, toUpper});
+    return (toUpper) ? value.toUpperCase() : value.toLowerCase();
   }
 
 }
