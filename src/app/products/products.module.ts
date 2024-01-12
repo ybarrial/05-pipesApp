fc8a4a332f78e267c0d3c3f62835dc82766b81ca
Your Name <yefferson.barrial@avivel.com.pe>
2024-01-12 15:55:27 -0500

    customPipe toggle-case

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -7,6 +7,7 @@ import { NumbersPageComponent } from './pages/numbers-page/numbers-page.componen
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
 
 
 
@@ -15,7 +16,10 @@ import { OrderComponent } from './pages/order/order.component';
     BasicsPageComponent,
     NumbersPageComponent,
     UncommonPageComponent,
-    OrderComponent
+    OrderComponent,
+
+    // Pipes
+    ToggleCasePipe,
   ],
   imports: [
     CommonModule,
