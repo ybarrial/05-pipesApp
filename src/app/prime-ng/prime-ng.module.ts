85a5d23851edc9b46f3e4e8928a7975559c54eb5
Your Name <yefferson.barrial@avivel.com.pe>
2024-01-12 16:30:01 -0500

    add modules of primeNg

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -6,21 +6,23 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    //MenuModule,
-    MenubarModule
   ],
   exports: [
-    //MenuModule,
-    MenubarModule,
+    // MenuModule,
     ButtonModule,
     CardModule,
     FieldsetModule,
-    PanelModule
+    MenubarModule,
+    PanelModule,
+    TableModule,
+    ToolbarModule,
   ]
 })
 export class PrimeNgModule { }
