bcc7f1fabe51c6180f9cfeaa9be9879be689a993
Your Name <yefferson.barrial@avivel.com.pe>
2024-01-18 10:27:50 -0500

    details

--- src/app/products/pipes/sort-by.pipe.ts
+++ src/app/products/pipes/sort-by.pipe.ts
@@ -16,7 +16,7 @@ export class SortByPipe implements PipeTransform {
         return heroes.sort( (a,b) => (a.name > b.name) ? 1 : -1 );
       case 'canFly':
         return heroes.sort( (a,b) => (a.canFly > b.canFly) ? 1 : -1 );
-      case 'canFly':
+      case 'color':
         return heroes.sort( (a,b) => (a.color > b.color) ? 1 : -1 );
       default:
         return heroes;
