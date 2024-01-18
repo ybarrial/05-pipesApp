6db918cd2b663bea5fa257382bb444b2f67b038c
Your Name <yefferson.barrial@avivel.com.pe>
2024-01-18 10:17:38 -0500

    add reference sortBy to module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -9,6 +9,7 @@ import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
 import { ToggleCasePipe } from './pipes/toggle-case.pipe';
 import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 
 
@@ -20,7 +21,8 @@ import { CanFlyPipe } from './pipes/can-fly.pipe';
     OrderComponent,
     // Pipes
     ToggleCasePipe,
-    CanFlyPipe
+    CanFlyPipe,
+    SortByPipe
   ],
   imports: [
     CommonModule,
